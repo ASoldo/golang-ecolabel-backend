0034bc6c70ca72932fff3d593379b78df9b11cc7
ASoldo <soldo.andrej@gmail.com>
2023-05-06 23:35:36 +0200

    Add custom logger and custom errors
    Add package for creating custom errors with specific format across whole application.
    Add package for logging custom log messages across whole application.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
+// "github.com/ASoldo/golang-ecolabel-backend/internal/errors"
 import (
 	"net/http"
 
+	"github.com/ASoldo/golang-ecolabel-backend/internal/logger"
 	"github.com/ASoldo/golang-ecolabel-backend/internal/routes"
 )
 
 func main() {
+	// err := errors.NewAppError(400, "Bad Request")
+	// logger.Error.Println(err)
+
 	r := routes.SetupRoutes()
+	logger.Info.Println("Application started")
 	http.ListenAndServe(":3000", r)
 }
